28e7971512dffdb527e59850c4d03373e83e7d0b
up240533 <up240533@alumnos.upa.edu.mx>
2025-06-17 19:44:46 -0600

    actualizé archivo

--- dia1.py
+++ dia1.py
@@ -1,2 +1,33 @@
-print("Dia 1")
-hola = 1
+# 1. Escribe tu nombre
+nombre = "Juan Manuel"
+
+# 2. Escribe tu apellido
+apellido = "Contreras"
+
+# 3. Escribe tu país
+pais = "México"
+
+# 4. Escribe tu edad
+edad = 21
+
+# 5. ¿Eres soltero/a o casado/a?
+estado_civil = "soltero"
+
+# 6. ¿Eres nuevo en Python?
+es_nuevo_en_python = False
+
+# 7. Declara múltiples variables en una sola línea
+nombre2, apellido2, pais2, edad2, estado_civil2 = "Cecilia", "Rodriguez", "Colombia", 22, "cassada"
+
+# 8. Usa la función `print()` para mostrar valores
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("País:", pais)
+print("Edad:", edad)
+print("Estado civil:", estado_civil)
+print("¿Nuevo en Python?", es_nuevo_en_python)
+
+# 9. Comprueba tipos de datos
+print(type(nombre))      # str
+print(type(edad))        # int
+print(type(es_nuevo_en_python))  # bool
